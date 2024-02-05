73e6f0db0c3fb8a5203e619ac7be78ff8b7517b7
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 16:58:37 +0100

    Initial commit

---
+++ src/main/java/de/jefa/garbage_application_security_server/GarbageApplicationSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package de.jefa.garbage_application_security_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageApplicationSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarbageApplicationSecurityServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/jefa/garbage_application_security_server/GarbageApplicationSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jefa.garbage_application_security_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageApplicationSecurityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
